47cfd5080f893f819caf00345fc88103d90a8d3f
skfareed-dev <fsk127699@gmail.com>
2025-12-22 16:24:41 +0530

    modified

--- vars/pushToRegistry.groovy
+++ vars/pushToRegistry.groovy
@@ -1,22 +1,16 @@
 def call(steps, Map config) {
-    steps.stage('Tag & Push to Nexus') {
+    steps.stage('Push to Nexus') {
         steps.withCredentials([steps.usernamePassword(
             credentialsId: config.credsId,
             usernameVariable: 'NEXUS_USER',
             passwordVariable: 'NEXUS_PASSWORD'
         )]) {
             steps.sh """
-            echo \$NEXUS_PASSWORD | docker login ${config.registry} \
-              -u \$NEXUS_USER --password-stdin
-
-            docker tag ${config.backendImage}:${config.tag} \
-              ${config.registry}/${config.repo}/${config.backendImage}:${config.tag}
-
-            docker tag ${config.frontendImage}:${config.tag} \
-              ${config.registry}/${config.repo}/${config.frontendImage}:${config.tag}
-
-            docker push ${config.registry}/${config.repo}/${config.backendImage}:${config.tag}
-            docker push ${config.registry}/${config.repo}/${config.frontendImage}:${config.tag}
+                echo $NEXUS_PASSWORD | docker login ${config.registry} -u $NEXUS_USER --password-stdin
+                docker tag ${config.backendImage}:${config.tag} ${config.registry}/${config.repo}/${config.backendImage}:${config.tag}
+                docker tag ${config.frontendImage}:${config.tag} ${config.registry}/${config.repo}/${config.frontendImage}:${config.tag}
+                docker push ${config.registry}/${config.repo}/${config.backendImage}:${config.tag}
+                docker push ${config.registry}/${config.repo}/${config.frontendImage}:${config.tag}
             """
         }
     }
