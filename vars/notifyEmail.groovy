383f001a7f31d37636d17e5266283e7e73bb2a8a
skfareed-dev <fsk127699@gmail.com>
2025-12-22 16:56:55 +0530

    modified

--- vars/notifyEmail.groovy
+++ vars/notifyEmail.groovy
@@ -1,16 +1,13 @@
 def call(String status) {
-    try {
-        emailext(
-            subject: "Jenkins Build ${status}: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
-            body: """
-            <h3>Build ${status}</h3>
-            <p><b>Job:</b> ${env.JOB_NAME}</p>
-            <p><b>Build Number:</b> ${env.BUILD_NUMBER}</p>
-            <p><b>Build URL:</b> <a href="${env.BUILD_URL}">${env.BUILD_URL}</a></p>
-            """,
-            to: "jsp82767@gmail.com"
-        )
-    } catch (e) {
-        echo "Email notification failed: ${e.getMessage()}"
-    }
+    emailext(
+        subject: "Build ${status}: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
+        body: """
+        <h3>Build ${status}</h3>
+        <p><b>Job:</b> ${env.JOB_NAME}</p>
+        <p><b>Build:</b> #${env.BUILD_NUMBER}</p>
+        <p><b>URL:</b> <a href="${env.BUILD_URL}">${env.BUILD_URL}</a></p>
+        """,
+        mimeType: 'text/html',
+        to: 'jsp82767@gmail.com'
+    )
 }
